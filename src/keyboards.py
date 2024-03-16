f820800beba95f14c804d11b06326ffbaece2e52
Artem <artem.rmb@bk.ru>
2024-03-16 13:55:10 +0300

    feat: added new keyboard for on boarding

--- src/keyboards.py
+++ src/keyboards.py
@@ -1,14 +1,30 @@
-from telegram import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telegram import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    WebAppInfo,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 
-from src.constants import ButtonText, WEB_APP_URL
+from src.constants import ButtonText, WEB_APP_URL, CallbackQuery
 
 MAIN_KEYBOARD = ReplyKeyboardMarkup(
     [
+        [KeyboardButton(ButtonText.TASKS, web_app=WebAppInfo(f"{WEB_APP_URL}/"))],
         [
-            KeyboardButton(ButtonText.TASKS, web_app=WebAppInfo(f"{WEB_APP_URL}/")),
             KeyboardButton(
                 ButtonText.PROFILE, web_app=WebAppInfo(f"{WEB_APP_URL}/profile")
-            ),
-        ]
+            )
+        ],
+    ],
+    resize_keyboard=True,
+)
+
+ON_BOARD_KEYBOARD = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(ButtonText.YES, callback_data=CallbackQuery.YES_REF),
+            InlineKeyboardButton(ButtonText.NO, callback_data=CallbackQuery.NO_REF),
+        ],
     ]
 )
