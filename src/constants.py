e3c92f13f3c34325fe813f56abda65fc70b977d6
Artem <artem.rmb@bk.ru>
2024-03-16 13:54:53 +0300

    feat: added new constants

--- src/constants.py
+++ src/constants.py
@@ -11,14 +11,27 @@ WEB_APP_URL = "https://33d9-178-66-131-169.ngrok-free.app"
 
 class PocketbaseCollections:
     USERS = "users"
+    ORGANIZATIONS = "organizations"
 
 
 class ButtonText:
     TASKS = "View tasks 💬"
     PROFILE = "Profile 👨‍💻"
+    YES = "Yes ✅"
+    NO = "No ❌"
 
 
 class BotReplies:
-    USER_NAME = "Type your name (ex. Name Surname)"
-    USER_NAME_UPLOADED = "Thanks for registration"
     ALSO_REGISTERED = "You also registered"
+    HAVE_REF = "Do you have invitation link to organization?"
+    SEND_REF_LINK = "Send your invitation link to join to organization"
+    NO_ORGANIZATIONS_BY_LINK = "Sorry, but no one organization was found!"
+    ADDED_TO_ORGANIZATION = (
+        "You was added to {organization_name} as a worker.\n Welcome to Stask!"
+    )
+    WELCOME = "Welcome to Stask!"
+
+
+class CallbackQuery:
+    YES_REF = "yes_ref"
+    NO_REF = "no_ref"
