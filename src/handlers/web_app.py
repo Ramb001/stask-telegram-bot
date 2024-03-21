46219f83b8d857b98cde47f8ec61f1301af79f9e
Artem <artem.rmb@bk.ru>
2024-03-21 21:14:36 +0300

    feat: created new handler for webapp

---
+++ src/handlers/web_app.py
@@ -0,0 +1,82 @@
+import html
+import json
+
+import aiohttp
+from amplitude import BaseEvent
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import ContextTypes
+
+from src.constants import (
+    PB,
+    BotReplies,
+    WebAppActions,
+    TaskStatuses,
+    PocketbaseCollections,
+)
+from src.helpers import fetch_organization, fetch_user
+
+
+async def web_app_data_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    action = json.loads(update.effective_message.web_app_data.data)
+    if action["action"] == WebAppActions.CREATE_TASK:
+        await __handle_create_task(update, context, action)
+
+
+async def __handle_create_task(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, action: dict
+):
+    data = action["data"]
+    async with aiohttp.ClientSession() as client:
+        organization = await fetch_organization(data["organization"], PB, client)
+        creator = await fetch_user(data["creator"], PB, client)
+
+        await PB.add_record(
+            PocketbaseCollections.TASKS,
+            client,
+            title=data["title"],
+            description=data["description"],
+            organization=organization["id"],
+            creator=creator["id"],
+            workers=[worker["id"] for worker in data["workers"]],
+            deadline=data["deadline"],
+            status=TaskStatuses.NOT_STARTED,
+        )
+
+        await context.bot.send_message(
+            update.effective_chat.id,
+            text=BotReplies.CREATED_TASK.format(
+                organization_name=data["organization"],
+                title=data["title"],
+                description=data["description"],
+                workers=", ".join(
+                    (
+                        worker["name"]
+                        if len(worker["name"]) != 0
+                        else f"@{worker['username']}"
+                    )
+                    for worker in data["workers"]
+                ),
+            ),
+            parse_mode=ParseMode.HTML,
+        )
+
+    for worker in data["workers"]:
+        if worker["id"] != organization["owner"]:
+            await context.bot.send_message(
+                worker["chat_id"],
+                text=BotReplies.CREATED_TASK.format(
+                    organization_name=data["organization"],
+                    title=data["title"],
+                    description=data["description"],
+                    workers=", ".join(
+                        (
+                            worker["name"]
+                            if len(worker["name"]) != 0
+                            else f"@{worker['username']}"
+                        )
+                        for worker in data["workers"]
+                    ),
+                ),
+                parse_mode=ParseMode.HTML,
+            )
