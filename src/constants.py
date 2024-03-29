dc1026fb86a3667a2d7aae66ba60f84d97278494
Artem <artem.rmb@bk.ru>
2024-03-29 17:41:57 +0300

    fix: fix

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,7 @@ from src.pocketbase import Pocketbase
 POCKETBASE_URL = "http://45.143.94.202:8090"
 PB = Pocketbase(POCKETBASE_URL)
 TOKEN = os.getenv("STASK_BOT_TOKEN")
-WEB_APP_URL = "https://9a9e-188-170-81-101.ngrok-free.app"
+WEB_APP_URL = "https://894c-178-66-158-106.ngrok-free.app"
 
 
 class PocketbaseCollections:
@@ -43,7 +43,7 @@ class BotReplies:
     )
     WELCOME = "Welcome to Stask!"
     CREATED_TASK = "<b>New task</b> in {organization_name}!\n<b>Title:</b> {title}\n<b>Description:</b> {description}\n<b>Workers:</b> {workers}"
-    CREATED_ORGANIZATION = "New organization - <b>{organization_name}</b> was created!\n<b>Organization referal link:</b> {ref_link}"
+    CREATED_ORGANIZATION = "New organization - <b>{organization_name}</b> was created!\n<b>Organization referal code:</b> <code>{ref_link}</code>"
 
 
 class CallbackQuery:
